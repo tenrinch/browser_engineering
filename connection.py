4e1b27af2e2c102deef34cbcb00b764083783f0b
tenrinch <rinchent95@gmail.com>
2023-03-28 06:53:00 -0400

    function for taking url and displaying html body on the commandline

--- connection.py
+++ connection.py
@@ -1,10 +1,11 @@
 import socket
+import sys
 
 def request(url):
 
     # make sure the url start with a http request
-    assert url.startwith('http://')
-    url = url[len('http://')]
+    assert url.startswith('http://')
+    url = url[len('http://'):]
 
     # splitting the host and path form the url
     host, path = url.split('/', 1)
@@ -26,7 +27,7 @@ def request(url):
 
     # splitting the response into pieces 
     statusline = response.readline()
-    status, version, explanation = statusline(' ', 2)
+    version, status, explanation = statusline.split(' ', 2)
     assert status == '200', '{}: {}'.format(status, explanation)
 
     # splitting the header line 
@@ -38,6 +39,10 @@ def request(url):
         headers[header.lower()] = value.strip() #normalize header to lower case and strip leading and trailing while spaces
 
     # assuring some kind of header are no present
+    """
+    Go further: 
+     The Content-Encoding header lets the server compress web pages before sending them. Large, text-heavy web pages compress well, and as a result the page loads faster. The browser needs to send an Accept-Encoding header in its request to list compression algorithms it supports. Transfer-Encoding is similar and also allows the data to be “chunked”, which many servers seem to use together with compression.
+    """
     assert 'transfer-encoding' not in headers
     assert 'content-encoding' not in headers
 
@@ -46,3 +51,23 @@ def request(url):
     s.close()
 
     return headers, body
+
+# displaying the body of the html
+def show(body):
+    in_angle = False
+    for c in body: 
+        if c == '<':
+            in_angle = True
+        if c == '>': 
+            in_angle = False
+        elif not in_angle:
+            print(c, end='')
+
+# load a web page just by stringing together request and show:
+def load(url):
+    headers, body = request(url)
+    show(body)
+
+# load the browser from command line, reads the first argument from the command line as the url
+if __name__ == "__main__":
+    load(sys.argv[1])
